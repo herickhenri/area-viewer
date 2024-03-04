5f5948fd3c7037c549b405c20da5f8bbc6ef632a
erickhenri <herick.henri16@gmail.com>
2024-03-04 10:32:14 -0300

    remove CLOUDFLARE_BASE_URL env

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,6 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
-  VITE_CLOUDFLARE_BASE_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(import.meta.env)

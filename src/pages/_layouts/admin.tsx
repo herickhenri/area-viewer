71cce8840697cb4f5d2e39c528cff0f5c929a4cf
erickhenri <herick.henri16@gmail.com>
2024-06-25 21:16:44 -0300

    add favicon

--- src/pages/_layouts/admin.tsx
+++ src/pages/_layouts/admin.tsx
@@ -1,17 +1,20 @@
 import { Link, Outlet } from 'react-router-dom'
 
-import logoSuzano from '@/assets/logo_suzano.png'
+import areaViewerLogo from '@/assets/area-viewer-logo.png'
 
 export function AdminLayout() {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="flex w-full items-center justify-between gap-8 px-4 py-4 shadow-lg">
+      <header className="flex w-full items-center justify-between gap-8 px-4 py-2 shadow-lg">
         <Link
           to="/admin"
-          className="flex items-end font-semibold text-blue-700"
+          className="flex items-center gap-2 text-xl font-semibold text-[#4D4D4D]"
         >
-          <img src={logoSuzano} alt="Logo da suzano" className="h-8" />
-          <h2>Admin</h2>
+          <img src={areaViewerLogo} alt="Logo da suzano" className="h-10" />
+          <span>
+            Area<span className="text-[#0082FC]">Viewer</span>
+            <span className="text-sm">Admin</span>
+          </span>
         </Link>
       </header>
 
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,13 +1,19 @@
 import { Link, Outlet } from 'react-router-dom'
 
-import logoSuzano from '@/assets/logo_suzano.png'
+import areaViewerLogo from '@/assets/area-viewer-logo.png'
 
 export function AppLayout() {
   return (
     <div className="flex min-h-screen flex-col">
-      <header className="flex w-full items-center justify-between gap-2 px-4 py-4 shadow-lg md:justify-normal">
-        <Link to="/">
-          <img src={logoSuzano} alt="Logo da suzano" className="h-8" />
+      <header className="flex w-full items-center justify-between gap-2 px-4 py-2 shadow-lg md:justify-normal">
+        <Link
+          to="/"
+          className="flex items-center gap-2 text-xl font-semibold text-[#4D4D4D]"
+        >
+          <img src={areaViewerLogo} alt="Logo da suzano" className="h-10" />
+          <span>
+            Area<span className="text-[#0082FC]">Viewer</span>
+          </span>
         </Link>
 
         <nav className="flex divide-x md:ml-10">

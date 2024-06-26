70db95c5f8289829b3595969b37626dd6bd4e5a0
erickhenri <herick.henri16@gmail.com>
2024-06-25 23:48:28 -0300

    create menubar

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,8 +1,12 @@
 import { Link, Outlet } from 'react-router-dom'
 
 import areaViewerLogo from '@/assets/area-viewer-logo.png'
+import { List, X } from '@phosphor-icons/react'
+import { useState } from 'react'
 
 export function AppLayout() {
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
     <div className="flex min-h-screen flex-col bg-gray-100">
       <header className="flex w-full items-center justify-between gap-2 bg-white px-4 py-2 shadow-lg md:justify-normal">
@@ -16,7 +20,7 @@ export function AppLayout() {
           </span>
         </Link>
 
-        <nav className="flex divide-x md:ml-10">
+        <nav className="ml-10  hidden divide-x md:flex">
           <Link to="/" className="px-2 py-2 font-semibold hover:bg-gray-100">
             Equipamentos
           </Link>
@@ -33,6 +37,41 @@ export function AppLayout() {
             Notas
           </Link>
         </nav>
+
+        <div className="block md:hidden">
+          <List
+            size={24}
+            className="cursor-pointer transition-all hover:text-blue-600"
+            onClick={() => setShowMenu(true)}
+          />
+          <div
+            className={`${showMenu ? '' : '-translate-x-full'} fixed inset-0 flex flex-col items-center justify-center gap-3 bg-white px-8 text-center text-2xl font-semibold`}
+          >
+            <X
+              size={24}
+              className="absolute right-4 top-4 z-10 cursor-pointer transition-colors hover:text-red-800"
+              onClick={() => setShowMenu(false)}
+            />
+            <Link
+              to="/"
+              className="w-full py-2 transition-colors hover:text-blue-600"
+            >
+              Equipamentos
+            </Link>
+            <Link
+              className="w-full py-2 transition-colors hover:text-blue-600"
+              to="panoramas"
+            >
+              Panoramas
+            </Link>
+            <Link
+              className="w-full py-2 transition-colors hover:text-blue-600"
+              to="notes"
+            >
+              Notas
+            </Link>
+          </div>
+        </div>
       </header>
 
       <Outlet />
